d2a7c3a3a32496c5f689126d01d415de148dd98e
James Chen <jchen@everlightradiology.com>
2021-08-20 11:04:25 +1000

    init

---
+++ JiejieChenApp/Extensions/NetCoreDiSetupExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.SimpleNotificationService;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+
+namespace JiejieChenApp.Extensions
+{
+    public static class NetCoreDiSetupExtensions
+    {
+        public static void RegisterServiceLayerDi(this IServiceCollection services)
+        {
+            services.Scan(scan => scan
+                .FromAssemblies(Assembly.GetAssembly(typeof(Startup)))
+                .AddClasses(classes => classes.Where(c => c.Name.EndsWith("Service")))
+                .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+                .AsImplementedInterfaces()
+                .WithTransientLifetime());
+        }
+
+        public static void RegisterAwsServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAWSService<IAmazonSimpleNotificationService>(ServiceLifetime.Transient);
+        }
+    }
+}
