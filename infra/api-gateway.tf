ad04fc9b98fbc8714237e89c9ca7127608b433fa
James Chen <james.chen@standards.org.au>
2024-01-08 23:49:32 +1100

    Added api.jiejiechen.com, use .NET 6 & 8 frameworks.

--- infra/api-gateway.tf
+++ infra/api-gateway.tf
@@ -1,10 +1,10 @@
-resource "aws_apigatewayv2_api" "hello-world" {
-  name          = "hello-world"
+resource "aws_apigatewayv2_api" "jiejiechen_api" {
+  name          = "jiejiechen-api"
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_stage" "default" {
-  api_id = aws_apigatewayv2_api.hello-world.id
+  api_id = aws_apigatewayv2_api.jiejiechen_api.id
 
   name        = "$default"
   auto_deploy = true
@@ -29,21 +29,21 @@ resource "aws_apigatewayv2_stage" "default" {
 }
 
 resource "aws_cloudwatch_log_group" "main_api_gw" {
-  name = "/aws/api-gw/${aws_apigatewayv2_api.hello-world.name}"
+  name = "/aws/api-gw/${aws_apigatewayv2_api.jiejiechen_api.name}"
 
   retention_in_days = 30
 }
 
 resource "aws_apigatewayv2_integration" "lambda_handler" {
-  api_id = aws_apigatewayv2_api.hello-world.id
+  api_id = aws_apigatewayv2_api.jiejiechen_api.id
 
   integration_type = "AWS_PROXY"
   integration_uri  = aws_lambda_function.handler.invoke_arn
 }
 
-resource "aws_apigatewayv2_route" "post_handler" {
-  api_id    = aws_apigatewayv2_api.hello-world.id
-  route_key = "POST /handler"
+resource "aws_apigatewayv2_route" "get_handler" {
+  api_id    = aws_apigatewayv2_api.jiejiechen_api.id
+  route_key = "GET /handler"
 
   target = "integrations/${aws_apigatewayv2_integration.lambda_handler.id}"
 }
@@ -54,5 +54,21 @@ resource "aws_lambda_permission" "api_gw" {
   function_name = aws_lambda_function.handler.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_apigatewayv2_api.hello-world.execution_arn}/*/*"
+  source_arn = "${aws_apigatewayv2_api.jiejiechen_api.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_domain_name" "jiejiechen_api" {
+  domain_name = var.api_domain_name
+
+  domain_name_configuration {
+    certificate_arn = var.api_domain_certificate_arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_apigatewayv2_api_mapping" "jiejiechen_api" {
+  api_id      = aws_apigatewayv2_api.jiejiechen_api.id
+  domain_name = aws_apigatewayv2_domain_name.jiejiechen_api.id
+  stage       = aws_apigatewayv2_stage.default.id
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -45,3 +45,11 @@ variable "default_tags" {
   description = "Default tags for resources created in module. Type is map so please use format: {\"key\"=\"value\"}"
   default     = {}
 }
+
+variable "api_domain_name" {
+  type        = string
+}
+
+variable "api_domain_certificate_arn" {
+  type        = string
+}
