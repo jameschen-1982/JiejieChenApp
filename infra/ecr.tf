f524c23fc0ed17c3650970055c9b03c001af679b
James Chen <james.chen@standards.org.au>
2024-06-01 23:39:50 +1000

    fwfw

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,14 +1,14 @@
-resource "aws_ecr_repository" "jiejiechen-angular" {
-  name                 = "jiejiechen-angular"
-  image_tag_mutability = "MUTABLE"
-  encryption_configuration {
-    encryption_type = "KMS"
-  }
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-  
-}
+# resource "aws_ecr_repository" "jiejiechen-angular" {
+#   name                 = "jiejiechen-angular"
+#   image_tag_mutability = "MUTABLE"
+#   encryption_configuration {
+#     encryption_type = "KMS"
+#   }
+#   image_scanning_configuration {
+#     scan_on_push = true
+#   }
+#   
+# }
 
 resource "aws_ecr_repository" "jiejiechen-cms" {
   name                 = "jiejiechen-cms"
