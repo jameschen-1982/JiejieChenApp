b10ae66e63ee4e7dcbf8b76be65d7f69652cfa20
James Chen <james.chen@standards.org.au>
2024-06-02 13:37:45 +1000

    Add angular test support

--- jiejiechen-angular/src/app/app.component.spec.ts
+++ jiejiechen-angular/src/app/app.component.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {StrapiCmsService} from "./services/strapi-cms.service";
+import {Title} from "@angular/platform-browser";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
+    const spy = jasmine.createSpyObj<StrapiCmsService>(['getHomePageContent']);
+
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      providers: [
+        Title,
+        AppComponent,
+        {provide: StrapiCmsService, useValue: spy}
+      ]
     }).compileComponents();
   });
 
@@ -19,12 +27,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('James Chen - Team Lead, Application Architect, Full Stack Software Developer - 0430 227 759');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.about-section h2').textContent).toContain('About Me');
   });
 });
