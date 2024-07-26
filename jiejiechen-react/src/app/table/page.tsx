0ce200fa2710ae80eefc086c28b69968b14baeef
James Chen <james.chen@standards.org.au>
2024-07-27 01:41:45 +1000

    Boilerplate UI

--- jiejiechen-react/src/app/table/page.tsx
+++ jiejiechen-react/src/app/table/page.tsx
@@ -11,13 +11,40 @@ export default function Page() {
       setData(res.data);
     });
   }, []);
-  return (<div className="mx-auto max-w-7xl p-6">
-      <h1>Hello, Home Page!</h1>
-      {data && data.map((item: any) => (
-        <div key={item.date}>
-          {item.date} - {item.temperatureC}
-        </div>
-      ))}
+  return (
+    <div className="mx-auto max-w-7xl p-6">
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Date
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Temperature
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Summary
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          {data && data.map((item: any) => (
+            <tr key={item.date} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {item.date}
+              </th>
+              <td className="px-6 py-4">
+                {item.temperatureC}
+              </td>
+              <td className="px-6 py-4">
+                {item.summary}
+              </td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
