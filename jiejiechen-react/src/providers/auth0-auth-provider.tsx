d8fd2aeeecf697015339549ef60e195b5c9b0fd2
James Chen <james.chen@standards.org.au>
2024-07-28 02:18:18 +1000

    Fix build error

--- jiejiechen-react/src/providers/auth0-auth-provider.tsx
+++ jiejiechen-react/src/providers/auth0-auth-provider.tsx
@@ -10,7 +10,14 @@ const oidcConfig = {
   redirect_uri: process.env.NEXT_PUBLIC_SPA_HOST,
   scope: "openid profile email",
   extraQueryParams: { audience: process.env.NEXT_PUBLIC_IDP_AUDIENCE },
-  userStore: new WebStorageStateStore({store: localStorage})
+  userStore: new WebStorageStateStore({store: window?.localStorage}),
+  onSigninCallback: (_user: unknown | void): void => {
+    window.history.replaceState(
+      {},
+      document.title,
+      window.location.pathname
+    )
+  }
 };
 
 export default function Auth0AuthProvider({children}: { children: React.ReactNode }) {
